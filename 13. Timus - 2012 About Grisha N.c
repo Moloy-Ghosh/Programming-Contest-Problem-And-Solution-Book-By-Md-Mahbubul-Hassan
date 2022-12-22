9ca7572d54b847eeffe443b62bbe7be68b43c4f3
Moloy-Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2022-12-22 22:03:43 +0600

    Add files via upload

---
+++ 13. Timus - 2012 About Grisha N.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int f,r;
+    scanf("%d",&f);
+    r=12-f;
+    if(((12-f)*45)<=240)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
